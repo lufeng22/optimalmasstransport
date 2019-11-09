587def0b1a94ef6ec71d3432ba9be5101e3f1a49
tuyanshuai <tuyanshuai@gmail.com>
2019-11-09 02:17:27 -0700

    change data mesh

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# optimalmasstransport
+# The Optimal Mass Transport for Mesh Process
 Basic tools  and some demo for Optimal mass transport
+
+## Area Preserving Mapping
+
+
+
+
+## Package Dependency
+trimesh==3.4.0
+numpy==1.16.4
+pyglet==1.4.0b1
+pyrender==0.1.32
+
+trimesh is used to load obj trimesh, and for a simple mesh show.
+
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import numpy as np
 import trimesh
 import pyrender
-mesh = trimesh.load('data/alex.obj')
+mesh = trimesh.load('data/face.obj')
 
 # (press w in viewer to see triangles)
-# mesh.show()
+mesh.show()
 
-mesh_render = pyrender.Mesh.from_trimesh(mesh)
-scene = pyrender.Scene()
-scene.add(mesh_render)
-pyrender.Viewer(scene, use_raymond_lighting=True)
+## if you wish a better render, use following
+# mesh_render = pyrender.Mesh.from_trimesh(mesh)
+# scene = pyrender.Scene()
+# scene.add(mesh_render)
+# pl = pyrender.PointLight(color=[1.0, 1.0, 1.0], intensity=2.0)
+#
+# pyrender.Viewer(scene, use_raymond_lighting=True)
+
